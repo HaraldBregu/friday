cbfe2145f4b31182729aa81c3a89306b77b562c9
Harald Bregu <harald.bregu@gmail.com>
2026-09-02 23:38:18 +0200

    auto: commit after tool 2026-09-02 23:38:18

--- tests/unit/preload/normalize.test.ts
+++
@@ -1,7 +0,0 @@
-import { optionalStringList } from '../../../src/preload/normalize';
-
-it('preserves an explicit empty string list', () => {
-	expect(optionalStringList([])).toEqual([]);
-	expect(optionalStringList([' read ', '', 'write'])).toEqual(['read', 'write']);
-	expect(optionalStringList(undefined)).toBeUndefined();
-});
