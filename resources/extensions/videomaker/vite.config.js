8d3f80e316d6442251fedfeff0d219a8b39b9a0e
Harald Bregu <harald.bregu@gmail.com>
2026-09-03 23:47:58 +0200

    auto: commit after tool 2026-09-03 23:47:58

--- resources/extensions/videomaker/vite.config.js
+++ resources/extensions/videomaker/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 	publicDir: false,
 	resolve: {
 		alias: {
-			'@kucedr/sdk': path.resolve(import.meta.dirname, '../../packages/sdk/index.ts'),
+			'@kucedr/sdk': path.resolve(import.meta.dirname, '../../../packages/sdk/index.ts'),
 		},
 	},
 });
