44c350d4e3753db698e27826f5f5f65f5745eef6
Harald Bregu <harald.bregu@gmail.com>
2026-09-03 23:51:23 +0200

    auto: commit after tool 2026-09-03 23:51:23

--- src/renderer/src/pages/settings/pages/cloud/Page.tsx
+++ src/renderer/src/pages/settings/pages/cloud/Page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { SettingsPageHeader, SettingsPageShell } from '../../components';
 import StoragePage from '../storage/Page';
+import Vault from './Vault';
 
 const CloudPage: React.FC = () => {
 	const { t } = useTranslation();
@@ -10,8 +11,9 @@ const CloudPage: React.FC = () => {
 		<SettingsPageShell>
 			<SettingsPageHeader
 				title={t('settings.tabs.cloud')}
-				description={t('settings.overview.descriptions.cloud')}
+					description={t('settings.overview.descriptions.cloud')}
 			/>
+			<Vault />
 			<StoragePage inline />
 		</SettingsPageShell>
 	);
