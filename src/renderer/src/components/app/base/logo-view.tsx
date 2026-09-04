91ff84582761e5d71b1cc4ba334c25802092d0a3
Harald Bregu <harald.bregu@gmail.com>
2026-09-04 14:06:21 +0200

    auto: commit after tool 2026-09-04 14:06:21

--- src/renderer/src/components/app/base/logo-view.tsx
+++ src/renderer/src/components/app/base/logo-view.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import icon from '@resources/icons/icon.png';
+import mark from '@resources/icons/kucedra-mark.svg';
 
 export function LogoView({
 	className = 'size-20 rounded-2xl',
 }: {
 	readonly className?: string;
 }): React.JSX.Element {
-	return <img src={icon} alt="Kucedr logo" className={`object-contain ${className}`} />;
+	return (
+		<span className={`inline-grid shrink-0 place-items-center overflow-hidden bg-black ${className}`}>
+			<img src={mark} alt="Kucedr logo" className="size-[80%] object-contain invert" />
+		</span>
+	);
 }
